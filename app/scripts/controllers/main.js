afd9df915f520a7e6ef3499b8716c944bae51ecb
Hugh Templeton <dippydoon100@yahoo.com>
2016-09-28 22:15:10 +0100

    Started adding args to main page (needs votes counts)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,18 @@
  * Controller of the letsArgueApp
  */
 angular.module('letsArgueApp')
-  .controller('MainCtrl', ["auth", "$scope", "$location", function (auth, $scope, $location) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+   .controller('MainCtrl', ["auth", "$scope", "$location", "$firebaseArray", "$timeout", function (auth, $scope, $location, $firebaseArray, $timeout) {
+
+    // synchronize a read-only, synchronized array of args, limit to most recent 10
+    var query = rootRef.child('args').limitToLast(10);
+    var args = $firebaseArray(query);
+    
+      args.$loaded()
+      .then(function () {
+        $scope.args = args;
+      })
+      .catch(alert);
 
     $scope.logout = function () {
       auth.$signOut();
@@ -21,5 +27,13 @@ angular.module('letsArgueApp')
       $location.path('/login');
       $scope.authData = null;
     };
+    
+    function alert(msg) {
+      $scope.err = msg;
+      console.log(msg);
+      $timeout(function () {
+        $scope.err = null;
+      }, 5000);
+    }
 
   }]);

c95289fb668c6fc082bd0eaf69855cc13dd290bc
Hugh Templeton <dippydoon100@yahoo.com>
2016-09-29 23:39:56 +0100

    Add chart to myargs

--- app/scripts/controllers/myargs.js
+++ app/scripts/controllers/myargs.js
@@ -9,16 +9,55 @@
 angular.module('letsArgueApp')
   .controller('Myargs', ["$scope", "currentAuth", "$firebaseArray", "$timeout", function ($scope, currentAuth, $firebaseArray, $timeout) {
     $scope.user = currentAuth;
+    
+    $scope.options = {
+            chart: {
+                type: 'pieChart',
+                height: 500,
+                x: function(d){return d.key;},
+                y: function(d){return d.y;},
+                showLabels: true,
+                duration: 500,
+                labelThreshold: 0.01,
+                labelSunbeamLayout: true,
+                legend: {
+                    margin: {
+                        top: 0,
+                        right: 0,
+                        bottom: 0,
+                        left: 0
+                    }
+                }
+            }
+        };
+
+        $scope.chartData = [
+            {
+                key: "Agree",
+                y: 0
+            },
+            {
+                key: "Disagree",
+                y: 0
+            }
+        ];
 
     // synchronize a read-only, synchronized array of args, limit to most recent 10
     var query = rootRef.child('args').limitToLast(10);
     var args = $firebaseArray(query);
 
-    args.$loaded()
-      .then(function () {
+    args.$loaded().then(function () {
         $scope.args = args;
-      })
+    })
       .catch(alert);
+      
+    var query = rootRef.child('args').orderByChild('userId').equalTo(currentAuth.uid);
+ 
+    var myargs = $firebaseArray(query);
+
+    myargs.$loaded().then(function () {
+        $scope.myargs = myargs;
+    }).catch(alert);
 
      // provide a method for adding a args
     $scope.addArg = function (newArg) {
@@ -33,6 +72,22 @@ angular.module('letsArgueApp')
         })
           .catch(alert);
       }
+    };
+    
+    $scope.updateChartScope = function (argee, disagree) {
+        
+        $scope.chartData = [
+            {
+                key: "Agree",
+                y: argee
+            },
+            {
+                key: "Disagree",
+                y: disagree
+            }
+        ];
+
+        
     };
 
     function alert(msg) {
